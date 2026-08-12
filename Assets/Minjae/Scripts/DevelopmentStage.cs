2197622daf2ea5b4d100df86e42119da428925ca
mincodeer <minjaekim010203@gmail.com>
2026-08-13 01:55:15 +1200

    Created dev flow panels

---
+++ Assets/Minjae/Scripts/DevelopmentStage.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DevelopmentStage : MonoBehaviour
+{
+    // Tags indicating the player's development stage
+    public enum DevelopmentStage
+    {
+        None,
+        ProjectSetup,
+        Coding,
+        Design,
+        Sound,
+        Debug,
+        Build,
+        Result
+    }
+}
---
+++ Assets/Minjae/Scripts/WorkMethod.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WorkMethod : MonoBehaviour
+{
+    // To state whether menual or AI
+    public enum
+    {
+        None,
+        Manual,
+        AI
+    }
+}
