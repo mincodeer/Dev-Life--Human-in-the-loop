e31afb81559629873e33a5ab6f722fff1e8ddc34
Chenxuan-Ma <mcx21390120@gmail.com>
2026-08-15 18:01:01 +1200

    SettingMenu

--- Assets/Chenxuan/scripts/SettingMenu.cs
+++ Assets/Chenxuan/scripts/SettingMenu.cs
@@ -7,4 +7,11 @@ public class SettingMenu : MonoBehaviour
     { settingspanel.SetActive(true); }
     public void CloseSettings()
     { settingspanel.SetActive(false); }
+    public void SetBGMVolume(float volume)
+    { Debug.Log("BGM Volume: "+ volume); 
+    }
+public void SetSFXVolume(float volume)
+{
+    Debug.Log("SFX Volume: "+ volume);
+}
 }
